18d96fc26daa77839627de05bd3c05eafc475b27
Shea Kelly <shea.kelly@lwb.org.au>
2025-12-03 20:59:57 +1100

    day 2 part 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -11,24 +11,13 @@ const parseInput = (type: "input" | "sample") => {
   return result;
 };
 
-const isInvalid = (id: number): boolean => {
-  const idString = String(id);
-  if (idString.length % 2 !== 0) {
-    return false;
-  }
-  const midPoint = idString.length / 2;
-  // console.log({ idString, midPoint });
-  const a = Number(idString.slice(0, midPoint));
-  const b = Number(idString.slice(midPoint, idString.length));
-  const invalid = a === b;
-  // console.log({ a, b, invalid });
-  return invalid;
-};
-
-const part1 = (input: { firstId: number; lastId: number }[]) => {
+const sumInvalid = (
+  input: { firstId: number; lastId: number }[],
+  isInvalid: (id: number) => boolean,
+) => {
   let total = 0;
   input.forEach(({ firstId, lastId }) => {
-    console.log({ firstId, lastId });
+    // console.log({ firstId, lastId });
     for (let id = firstId; id <= lastId; id++) {
       if (isInvalid(id)) {
         total += id;
@@ -38,8 +27,44 @@ const part1 = (input: { firstId: number; lastId: number }[]) => {
   return total;
 };
 
-const part2 = (input: number[]) => {};
+const repeatsSequenceTwice = (id: number): boolean => {
+  const idString = String(id);
+  if (idString.length % 2 !== 0) {
+    return false;
+  }
+  const midPoint = idString.length / 2;
+  const a = Number(idString.slice(0, midPoint));
+  const b = Number(idString.slice(midPoint, idString.length));
+  const invalid = a === b;
+  return invalid;
+};
+
+const splitEvery = (text: string, every: number): string[] => {
+  const result: string[] = [];
+  const chars = Array.from(text);
+  for (let i = 0; i < chars.length; i = i + every) {
+    result.push(text.slice(i, i + every));
+  }
+  return result;
+};
+
+const hasRepeatingSeqOfLength = (text: string, seqLength: number): boolean => {
+  const [first, ...rest] = splitEvery(text, seqLength);
+  const result = rest.every((x) => x === first);
+  return result;
+};
+
+const repeatsSequenceAtLeastTwice = (id: number): boolean => {
+  const idString = String(id);
+  const len = idString.length;
+  for (let i = 1; i <= Math.floor(len / 2); i++) {
+    if (hasRepeatingSeqOfLength(idString, i)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 const input = parseInput("input");
-console.log("part1", part1(input));
-// console.log("part2", part2(input));
+console.log("part1", sumInvalid(input, repeatsSequenceTwice));
+console.log("part2", sumInvalid(input, repeatsSequenceAtLeastTwice));
